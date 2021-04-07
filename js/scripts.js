6243583e03bba28b6d02f3523015e218d9fe3302
Gabriel Ayala <gabeayala100@gmail.com>
2021-04-07 15:34:47 -0700

    Add code from lesson 28 for additions calculator.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+undefined
+const number1 = parseInt(prompt("Enter a number:"));
+undefined
+const number2 = parseInt(prompt("Enter another number:"));
+
+undefined
+alert(add(number1, number2));
+undefined
